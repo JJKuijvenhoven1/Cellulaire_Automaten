b79744cf1556b30038d88110c6c2e67a4972cf79
JJKuijvenhoven1 <jochem.kuijvenhoven@gmail.com>
2021-06-21 13:10:40 +0200

    overzicht + parent opzet

---
+++ Code.py
@@ -0,0 +1,25 @@
+class cellulair_automata():
+    def __init__(self):
+        return
+    
+    def regels(self):
+        print('this is an ordinary celluair automaton')
+        return
+    
+    def evolueer(self):
+        self.regels()
+        return
+    
+    
+class game_of_life(cellulair_automata):
+    def __init__(self):
+        return
+    def regels(self):
+        print('this is the game of life')
+        
+        
+
+x = game_of_life()
+x.evolueer()
+y = cellulair_automata()
+y.evolueer()  
