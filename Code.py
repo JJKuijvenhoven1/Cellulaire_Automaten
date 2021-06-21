8c30b26c4119574bad5a1cb45e70fe9bed25a91b
JJKuijvenhoven1 <jochem.kuijvenhoven@gmail.com>
2021-06-21 17:51:07 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,5 +1,8 @@
 class cellulair_automata():
-    def __init__(self):
+    def __init__(self, dimensions, startgrid, listofneighbours):
+        self.dimensions     = dimensions
+        self.grid           = startgrid
+        self.lisofneighbours = listofneighbours
         return
     
     def regels(self):
@@ -12,14 +15,17 @@ class cellulair_automata():
     
     
 class game_of_life(cellulair_automata):
-    def __init__(self):
+    def __init__(self,startgrid):
+        self.dimensions     = 2
+        self.grid           = startgrid
+        self.lisofneighbours = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,-1]]
         return
     def regels(self):
         print('this is the game of life')
         
         
 
-x = game_of_life()
+x = game_of_life(None)
 x.evolueer()
-y = cellulair_automata()
+y = cellulair_automata(None,None,None)
 y.evolueer()  
