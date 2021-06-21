5149e11c7d43154d038d6e7c40c1a725b8b56b2c
JJKuijvenhoven1 <jochem.kuijvenhoven@gmail.com>
2021-06-21 18:34:22 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,17 +1,27 @@
 class cellulair_automata():
-    def __init__(self, dimensions, startgrid, listofneighbours):
-        self.dimensions     = dimensions
-        self.grid           = startgrid
-        self.lisofneighbours = listofneighbours
-        return
+    def __init__(self):
+        #Deze variablen staan nu op de standaardwaarden zodat het 
+        #programma geen errors geeft maar kunnen bij de subclasses 
+        #ingevoerd worden op wat ze moeten zijn.
+        self.dimensions     = 1
+        self.grid           = [0]
+        self.lisofneighbours = [0]
+        
     
-    def regels(self):
-        print('this is an ordinary celluair automaton')
-        return
+    def regels(self,coordinaten):
+        #Deze functie bestaat puur en alleen om door de child objecten overschreven te worden.
+        print('this is the base celluair automaton')
+        nieuwetoestand = 0
+        return nieuwetoestand #we returnen de nieuwe toestand van het inputvakje
+        
     
     def evolueer(self):
-        self.regels()
-        return
+        #deze functie wordt door alle subclasses gebruikt en looped door elke entry
+        #van het grid en voert op dat vakje de functie regels uit
+        self.regels([0])
+        
+    
+#------------------------------------------------------------------------------    
     
     
 class game_of_life(cellulair_automata):
@@ -20,12 +30,14 @@ class game_of_life(cellulair_automata):
         self.grid           = startgrid
         self.lisofneighbours = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,-1]]
         return
-    def regels(self):
+    def regels(self,coordinaten):
         print('this is the game of life')
+        nieuwetoestand = 0
+        return nieuwetoestand
         
-        
-
-x = game_of_life(None)
+#-----------------------------------------------------------------------------    
+#testcode om het verschil tussen de automata te laten zien
+x = game_of_life([[0]])
 x.evolueer()
-y = cellulair_automata(None,None,None)
+y = cellulair_automata()
 y.evolueer()  
